d812d0abaa149889a46f98caca26f0da78ee01e6
younikingneerajgupta <neerajgupta5963@gmail.com>
2023-10-30 22:09:18 +0530

    modify urls.py

--- lifemastry/urls.py
+++ lifemastry/urls.py
@@ -16,3 +16,5 @@ urlpatterns = [
 # Serve media files during development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
